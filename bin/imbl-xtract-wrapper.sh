f9008f2219a8ce2411f32b61533213de5a5e5d7e
Your Name <you@example.com>
2018-06-30 17:06:42 +1000

    modified:   bin/imbl-xtract-wrapper.sh

--- bin/imbl-xtract-wrapper.sh
+++ bin/imbl-xtract-wrapper.sh
@@ -16,10 +16,9 @@ chkf () {
   fi
 }
 
-projFiles=""
+projFiles="SAMPLE\w+.tif"
 recFiles=""
 sinFiles=""
-chkf "$initfile" init
 
 while getopts "p:r:s:h" opt ; do
   case $opt in
@@ -58,7 +57,7 @@ fi
 if ! mkdir -p "${outdir}" ; then
   echo "Could not create output directory \"${outdir}\"." >&2
   exit 1
-fi
+i
 
 xparams="$(cat "$(realpath "$xtParamFile")" |
             perl -p -e 's/:\n/ /g' |
@@ -69,18 +68,40 @@ xparams="$(cat "$(realpath "$xtParamFile")" |
             grep -v -- --outdir)"
 
 if [ ! -z "$projFiles" ] ; then
+ 
+    if [ ! -z "$recFiles" ] ; then
+        xparams="$( echo "$xparams" | grep -v -- --file_prefix_ctrecon)" \
+                " --file_prefix_ctrecon \"$recFiles\""
+    fi
+    if [ ! -z "$sinFiles" ] ; then
+        xparams="$( echo "$xparams" | grep -v -- --file_prefix_sinograms)" \
+                " --file_prefix_sinograms \"$sinFiles\""
+    fi
     xparams="$( echo "$xparams" | grep -v -- --proj)" \
             " --proj \"$projFiles\""
+
+    drop_caches
+    xlictworkflow_local.sh $xparams 
+    exit $?
+
 fi
-if [ ! -z "$recFiles" ] ; then
+
+nsplits=$(ls clean/SAMPLE*split* | sed 's .*\(_split[0-9]\+\).* \1 g' | sort | uniq)
+if [ -z "$nsplits" ] && ! ls clean/SAMPLE*tif > /dev/null ; then
+  nsplits="_"
+fi
+
+for spl in $nsplits ; do
+
     xparams="$( echo "$xparams" | grep -v -- --file_prefix_ctrecon)" \
-            " --file_prefix_ctrecon \"$recFiles\""
-fi
-if [ ! -z "$sinFiles" ] ; then
+                " --file_prefix_ctrecon \"recon${spl}_.tif\""
     xparams="$( echo "$xparams" | grep -v -- --file_prefix_sinograms)" \
-            " --file_prefix_sinograms \"$recFiles\""
-fi
+                " --file_prefix_sinograms \"sino${spl}_.tif\""
+    xparams="$( echo "$xparams" | grep -v -- --proj)" \
+            " --proj \"SAMPLE\w*$spl\w*.tif\""
 
-drop_caches
+    drop_caches
+    xlictworkflow_local.sh $xparams 
 
-xlictworkflow_local.sh $xparams
+done
+
